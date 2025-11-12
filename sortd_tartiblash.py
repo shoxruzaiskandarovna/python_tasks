040cbe20140b2a6822181ea890fa007b5cb14548
shoxruza <otanazarovashoxruzaa@gmail.com>
2025-11-12 21:31:34 +0300

    Create sortd_tartiblash.py

---
+++ sortd_tartiblash.py
@@ -0,0 +1,25 @@
+cars = ['bmw','mercedes benz' , 'volvo', 'general motors', 'tesla', 'audi']
+cars.sort()
+print(cars)
+
+cars =  ['bmw','mercedes benz' , 'volvo', 'general motors', 'tesla', 'audi']
+print("sorted()(qaytargan royxat:", sorted(cars,reverse=True))
+print("Asl ro`yxat o`zgarmas qoldi:", cars)
+
+ages = [12,78,34,65,34,76,11]
+# ages.sort()
+# print(ages)
+print(sorted(ages, reverse=True))
+fruits = ['pear','banan', 'apple', 'watermelon', 'lemon']
+fruits.reverse()
+print(fruits)
+
+fruits = ['pear', 'banan', 'apple','watremelon', 'lemon']
+fruits.reverse()
+print(fruits)
+print(f"Fruits ro`yxat uzunligi:{len(fruits)}")
+
+sonlar = list(range(50,70))
+print(sonlar)
+print(len(sonlar))
+
